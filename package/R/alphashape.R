9b87c8567640943a39d7971f037f693274e28550
LANDCARE\omondiagbep <omondiagbep@landcareresearch.co.nz>
2020-02-10 11:19:39 +1300

    adding repo

---
+++ package/R/alphashape.R
@@ -0,0 +1,17 @@
+
+#' @title Computation of n dimension \eqn{\alpha}-shape 
+#'
+#' @description Implementation in n dimension of the alpha shape using the Q-hull library
+#'
+#' \tabular{ll}{ Package: \tab alphashape\cr  Date: \tab
+#' 2019-03-14\cr License: \tab GPL-2\cr LazyLoad: \tab yes\cr }
+#'
+#' @name alphashape
+#' @docType package
+#' @author Pascal Omondiagbe\cr Tom Etherington\cr Maintainers: pascal Omondiagbe <omondiagbep@landcareresearch.co.nz>
+#' @references http://www.qhull.org/html/qh-code.htm
+#' @keywords package
+#' @useDynLib alphashape
+NULL
+
+
