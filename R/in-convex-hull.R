4d96a50b10a654ed795bc18880bc2b40bd86a8b1
tretherington <tretherington@users.noreply.github.com>
2020-04-23 10:25:10 +1200

    Fixes to in_convex_hull

--- R/in-convex-hull.R
+++ R/in-convex-hull.R
@@ -1,19 +1,21 @@
-##' @title in convex hull
-##'
-##' @description  To test if a given point is inside the convex hull. \code{TRUE}  if the point  lies within the hull and \code{FALSE} if it
-##' lies outwith the hull 
-##' 
-##' @param  hull object Convex hull simplices produced using convex function
-##' @param points: dataframe or matrix  \code{n}-by-\code{dim} of points to check. 
-##' @return A \code{n*m} vector containing the result. True if a given point was inside the convexhull , otherwise false
-##' @examples 
-##' x = c(30,70,20,50,40,70)
-##' y = c(35,80,70,50,60,20)
-##' p =data.frame(x,y)
-##' convex =in_convex_hull(point=p)
-##' #point to check
-##' p2 = data.frame(x,y)
-##' in_convex_hull(convex,p2)
+#' @title in convex hull
+#'
+#' @description  To test if a given point is inside the convex hull. \code{TRUE}  if the point  lies within the hull and \code{FALSE} if it
+#' lies outwith the hull 
+#' 
+#' @param  hull object Convex hull simplices produced using convex function
+#' @param points: dataframe or matrix  \code{n}-by-\code{dim} of points to check. 
+#' @return A \code{n*m} vector containing the result. True if a given point was inside the convexhull , otherwise false
+#' @examples 
+#' # Define points and create convex hull
+#' x <- c(30, 70, 20, 50, 40, 70)
+#' y <- c(35, 80, 70, 50, 60, 20)
+#' p <- data.frame(x, y)
+#' ch <- convex_hull(points = p)
+#' # Check if some new points are in the convex hull
+#' new = data.frame(c(20, 50), c(20, 50))
+#' checks <- in_convex_hull(hull = ch, points = new)
+#' 
 #' @export in_convex_hull
 in_convex_hull <- function(hull, points) {
 	
@@ -37,6 +39,18 @@ in_convex_hull <- function(hull, points) {
 # REPLACED WITH
   # Using the convex hull lists recreate the convex hull in C
   options <- "Qt"
+  # Check directory writable
+	tmpdir <- tempdir()
+
+	# R should guarantee the tmpdir is writable, but check in any case
+	if (file.access(tmpdir, 2) == -1) {
+		stop(paste("Unable to write to R temporary directory", tmpdir, "\n"))
+	}
+	
+	
+	
+	
+	
   convex_hull <- .Call("C_convex", hull$hull_points, as.character(options), tmpdir, PACKAGE="alphashape")
 
 	return(.Call("C_inconvexhull", convex_hull, points, PACKAGE="alphashape"))
