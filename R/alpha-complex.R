0901574a9716493c09b7ddecc7c1cbac8282a150
tretherington <tretherington@users.noreply.github.com>
2020-04-29 08:55:05 +1200

    Added alpha complex function

--- R/alpha-complex.R
+++ R/alpha-complex.R
@@ -1,15 +1,22 @@
-#' @title Voronoi diagram
+#' @title Alpha complex
 #' 
 #' @description  This function calculates the 
-#' \href{https://en.wikipedia.org/wiki/Voronoi_diagram}{Voronoi digram} of a set
+#' \href{https://en.wikipedia.org/wiki/Alpha_shape}{alpha complex} of a set
 #' of \eqn{n} points in \eqn{d}-dimensional space using the
 #' \href{http://www.qhull.org}{Qhull} library.
 #' 
 #' @param points a \eqn{n}-by-\eqn{d} dataframe or matrix. The rows
 #'   represent \eqn{n} points and the \eqn{d} columns the coordinates in 
 #'   \eqn{d}-dimensional space.
+#' @param alpha a positive number that defines the maximum circumradii for a 
+#'   simplex to be included in the alpha complex.
 #' 
-#' @return Returns a list consisting of...
+#' @return Returns a list consisting of: [1] a \eqn{n}-by-\eqn{d+1} matrix of 
+#' point indices that define the 
+#' \href{https://en.wikipedia.org/wiki/Simplex}{simplices} that make up the
+#' alpha complex; [2] a list circumcentres for each simplex ; [3] a list of 
+#' circumradii for each simplex; and [4] the input points used to create the 
+#' alpha complex.
 #' 
 #' @references Barber CB, Dobkin DP, Huhdanpaa H (1996) The Quickhull algorithm 
 #' for convex hulls. ACM Transactions on Mathematical Software, 22(4):469-83 
@@ -20,11 +27,11 @@
 #' x <- c(30, 70, 20, 50, 40, 70)
 #' y <- c(35, 80, 70, 50, 60, 20)
 #' p <- data.frame(x, y)
-#' # Create Voronoi diagram and plot
-#' vd <- voronoi(points = p)
+#' # Create alpha complex and plot
+#' alpha_complex <- voronoi(points = p)
 #' plot(p, pch = as.character(seq(nrow(p))))
 #' @export
-voronoi <- function(points=NULL) {
+alpha_complex <- function(points=NULL, alpha=NULL) {
 	
     # Check directory writable
     tmpdir <- tempdir()
@@ -64,21 +71,20 @@ voronoi <- function(points=NULL) {
     vd$tri[is.na(vd$tri)] <- 0
     tri <- vd$tri + 1
     
-    # Create list to return the desired Voronoi diagram information
-    voronoi <- list()
-    voronoi$tri <- tri
-	  if (nrow(voronoi$tri) == 1) 
-	  {		
-	    voronoi$neighbours <- NULL
-	    voronoi$voronoi_vertices <- NULL
-	    voronoi$circumradii <- NULL
-	    
-	  }else{
-	    voronoi$neighbours <- vd$neighbours
-	    voronoi$voronoi_vertices <- vd$voronoi_vertices
-	    voronoi$circumradii <- vd$circumRadii
+    # Create list to return the desired alpha complex information
+    alpha_complex <- list()
+    in_alpha_complex <- vd$circumRadii <= alpha
+    
+    alpha_complex$tri <- tri[in_alpha_complex]
+    
+    if (nrow(alpha_complex$tri) < 1) {
+	    alpha_complex$circumcentres <- NULL
+	    alpha_complex$circumradii <- NULL
+	    } else {
+	    alpha_complex$circumcentres <- vd$voronoi_vertices[in_alpha_complex]
+	    alpha_complex$circumradii <- vd$circumRadii[in_alpha_complex]
 	  }
-	  voronoi$input_points <- points
+	  alpha_complex$input_points <- points
 
-  	return(voronoi)
+  	return(alpha_complex)
   }
