c348d6be1a4147b0602083d5a4d10cb7148b5bdb
LANDCARE\omondiagbep <omondiagbep@landcareresearch.co.nz>
2020-04-30 13:55:42 +1200

    updated find_simplex and alpha shape function to follow thesame convention

--- R/alpha-shape.R
+++ R/alpha-shape.R
@@ -16,22 +16,26 @@ NULL
 
 
 
-##' @title alpha_shape
-##' @description Compute an alpha Shape Grid using the Q-hull library. 
-##' @param point observation as dataframe or matrix
-##' @param alphaRange, range of alpha value
-##' @param mins Vector of length \code{n} listing the point space minimum for each dimension.
+#' @title alpha_shape
+#' @description Compute an alpha Shape Grid using the Q-hull library. 
+#' @param simplicies A Delaunay trigulation list object created by 
+#' \code{\link{delaunay}} or a alpha complex list object created by 
+#' \code{\link{alpha_complex}}.
+#' @param alpha_range, range of alpha value
+#' @param mins Vector of length \code{n} listing the point space minimum for each dimension.
 #'  @param maxs Vector of length \code{n} listing the point space maximum for each  dimension.
-##' @param n \code{n} dimension point co-ordinate
-##' @details The calculation is done by assigning the trigulation index when the grid cell center lies within the trigulation or -1 if it lies outside 
-##' @return grid stack as vector, gridSimplex, and the inputted grid point.
-##' @examples 
-##'  x = c(30,70,20,50,40,70,20)
-##'  y = c(35,80,70,50,60,20,30)
-##'  p = data.frame(x,y)
-##'  alpha_shape(point = p,maxs = c(70,80),mins = c(20,20),n = 5,alphaRange = c(1:20))
+#' @param n \code{n} dimension point co-ordinate
+#' @details The calculation is done by assigning the trigulation index when the grid cell center lies within the trigulation or -1 if it lies outside 
+#' @return grid stack as vector, gridSimplex, and the inputed grid point.
+#' @examples 
+#' # Define points and create a Delaunay triangulation
+#' x <- c(30, 70, 20, 50, 40, 70)
+#' y <- c(35, 80, 70, 50, 60, 20)
+#' p <- data.frame(x, y)
+#' a_complex <- alpha_complex(points = p, alpha = 20)
+#' alpha_shape(simplicies = a_complex,maxs = c(70,80),mins = c(20,20),n = 5,alpha_range = c(1:20))
 #' @export alpha_shape
-alpha_shape <- function(point,alphaRange,maxs,mins,n) {
+alpha_shape <- function(simplicies,alpha_range,maxs,mins,n) {
   
   tmpdir <- tempdir()
   error= list()
@@ -41,24 +45,23 @@ alpha_shape <- function(point,alphaRange,maxs,mins,n) {
     stop(paste("Unable to write to R temporary directory", tmpdir, "\n"))
   }
   
-  if(typeof(alphaRange) != "integer"){
+  if(typeof(alpha_range) != "integer"){
     error=append(error,paste0("alpha range must be a list or vector"))
   }else{
-    alphaRange=as.list(alphaRange)
+    alpha_range=as.list(alpha_range)
   }
   
   if( is.null(n)){
     error=append(error,paste0("n value cant be Null", "\n"))
   }
   
-  #check point
-  if (!is.data.frame(point) || !is.matrix(point) ){
-    error=append(error,paste0("Point must be a dataframe or matrix", "\n"))
-  }
+  #retrieve point 
+  
+  point= simplicies$input_points
+  
   
   #check min/mx
-  
-  # Check input data
+
   if (length(mins) != length(maxs)) {
     error=append(error,paste0("Length of mins and maxs differ"))
   }
@@ -66,35 +69,28 @@ alpha_shape <- function(point,alphaRange,maxs,mins,n) {
     error=append(error,paste0("Maximums not greater than minimums in all dimensions"))
   }
   
-  
-  nPoints= nrow(point)
-  pDim =length(point)
+
+  p_dim =length(point)
   
   #check to make sure the mins/max value specify is the same as the point dimension length
-  if(length(mins) != length(point)){
-    error=append(error,paste0("Length of mins differ from point dimension length. Please specify mins of length", pDim))
+  if(length(mins) != ncol(point)){
+    error=append(error,paste0("Length of mins differ from input point dimension length. Please specify mins of length", ncol(point)))
   }
   
-  if(length(maxs) != length(point)){
-    error=append(error,paste0("Length of maxs differ from point dimension length. Please specify maxs of length", pDim))
-  }
-  
-  #check to make sure we have the required point for the number of dimension specify, hence Qhull will return an error
-  nPointRequired = pDim +2
-  if(nPoints < nPointRequired){
-    error=append(error,paste0("You need at least ", nPointRequired, " point for ", pDim , "  dimension"))
+  if(length(maxs) != ncol(point)){
+    error=append(error,paste0("Length of maxs differ from input point dimension length. Please specify maxs of length", ncol(point)))
   }
   
   
   
-  #check to make sure the min/max are within the point
+  #check to make sure the min/max are within the input point
   p= c(1:length(maxs))
   for(i in p){
-    if(mins[i] < min(point[i])){
+    if(mins[i] < min(point[,i])){
       error=append(error,paste0("The min value specify in the position ", i ," of the mins vector is less than the minimum value specify in column ", i , " of the dataframe"))
     }
     
-    if(maxs[i] > max(point[i])){
+    if(maxs[i] > max(point[,i])){
       error=append(error,paste0("The max value specify in the position ", i ," of the maxs vector is greater than the minimum value specify in column ", i , " of the dataframe"))
     }
   }
@@ -104,48 +100,39 @@ alpha_shape <- function(point,alphaRange,maxs,mins,n) {
   
   if(length(error) ==0){
     
-    #get voronoi object
-    voronoiObject = voronoi(point=point)
-    
-    if(!is.matrix(point)){
-      point <- as.matrix(point)
-    }
+    #get alpha complex
+
+    if(is.vector(alpha_range))
+      alpha_range <- as.list(alpha_range) #change alpha range value to list
     
     
-    if(is.vector(alphaRange))
-      alphaRange <- as.list(alphaRange) #change alpha range value to list
-    
-    
-    dim = ncol(voronoiObject$voronoiVertices) # we can get the number of dimension from the Number of col on the voronoi vertices list
-    tri = voronoiObject$tri
-    circumRadii = voronoiObject$circumRadii
-    inputPoint=voronoiObject$inputPoints
-    center = voronoiObject$voronoiVertices
+    dim = ncol(simplicies$circumcentres) # we can get the number of dimension from the Number of col on the voronoi vertices list
+    circum_radii = simplicies$circumradii
+    center = simplicies$circumcentres
     colD = dim+1
     
     
     #create mesh grid
-    meshGrdiSpace = grid_coordinates(mins, maxs, nCoords=n)
+    mesh_grid_space = grid_coordinates(mins, maxs, nCoords=n)
     
     
     #Secondly, each point which is inside the convex hull is now check to determine if the center lies in any of the trigulation, and if found the trigulation index is return
-    #Secondly, each point which is inside the convex hull is now check to determine if the center lies in any of the trigulation, and if found the trigulation index is return
-    gridSpaceSimplex <- find_simplex(tri, inputPoint,meshGrdiSpace)
+    grid_space_simplex <- find_simplex(simplicies,mesh_grid_space)
     
     
-    alphaComplexSimplicesList =NULL
-    resultList=list()
-    emptyGrid <- ifelse(gridSpaceSimplex !=0,0,gridSpaceSimplex)
+    alpha_complex_simplices_list =NULL
+  
+    empty_grid <- ifelse(grid_space_simplex !=0,0,grid_space_simplex)
     #for each alpha , Identify those simplices that will form the alpha-shape
-    for(alpha in alphaRange){	
-      alphaComplexSimplices= which(circumRadii<alpha)
-      alphaComplexSimplices =alphaComplexSimplices -1
+    for(alpha in alpha_range){	
+      alpha_complex_simplices= which(circum_radii<alpha)
+      alpha_complex_simplices =alpha_complex_simplices -1
       
       j=1
       sp=NULL
-      for(value in gridSpaceSimplex){
+      for(value in grid_space_simplex){
         
-        if(! value %in% alphaComplexSimplices)
+        if(! value %in% alpha_complex_simplices)
           sp[j] = 0
         else{
           sp[j]=1
@@ -153,37 +140,16 @@ alpha_shape <- function(point,alphaRange,maxs,mins,n) {
         
         j<-j+1
       }
-      emptyGrid =emptyGrid+sp
+      empty_grid =empty_grid+sp
       
     }
     #return alpha grid as matrix
-    alphaGridMatrix = matrix(emptyGrid ,ncol=n,byrow=TRUE)
-    return (list(alphaGrid=alphaGridMatrix,gridSpace=meshGrdiSpace,gridSpaceSimplex =gridSpaceSimplex))
+    alpha_grid_matrix = matrix(empty_grid ,ncol=n,byrow=TRUE)
+    return (list(alphaGrid=alpha_grid_matrix,gridSpace=mesh_grid_space,gridSpaceSimplex =grid_space_simplex))
     
   }else{
     print(error)
   }
   
   
-}
-
-
-#Internal function use by \code{\link{findSimplex}} to calculate the bycentri co-ordinate
-get_ny_centri_coordinate <- function(X, P) {
-  M <- nrow(P)
-  N <- ncol(P)
-  if (ncol(X) != N) {
-    stop("Simplex X must have same number of columns as point matrix P")
-  }
-  if (nrow(X) != (N+1)) {
-    stop("Simplex X must have N columns and N+1 rows")
-  }
-  X1 <- X[1:N,] - (matrix(1,N,1) %*% X[N+1,,drop=FALSE])
-  if (rcond(X1) < .Machine$double.eps) {
-    warning("Degenerate simplex")
-    return(NULL)
-  }
-  Beta <- (P - matrix(X[N+1,], M, N, byrow=TRUE)) %*% solve(X1)
-  Beta <- cbind(Beta, 1 - apply(Beta, 1, sum))
-  return(Beta)
 }
--- R/find-simplex.R
+++ R/find-simplex.R
@@ -19,93 +19,91 @@
 #' x <- c(30, 70, 20, 50, 40, 70)
 #' y <- c(35, 80, 70, 50, 60, 20)
 #' p <- data.frame(x, y)
-#' dt <- delaunay(points = p)
+#' a_complex <- alpha_complex(points = p, alpha = 20)
 #' # Check which simplex the test points belong to
 #' p_test <- data.frame(c(20, 50, 60), c(20, 50, 60))
-#' simplex <- find_simplex(simplicies = dt, test_points = p_test)
+#' simplex <- find_simplex(simplicies = a_complex, test_points = p_test)
 #'  
 #' @export
-find_simplex <- function(tri,inputPoint,testPoint) {
+find_simplex <- function(simplicies,test_points) {
   
-  if(!is.data.frame(testPoint)){
-    stop("please supply a test point dataframe")
+  # Coerce the input to be matrix
+  if(is.null(test_points)){
+    stop(paste("points must be an n-by-d dataframe or matrix", "\n"))
+  }
+  if(!is.data.frame(test_points) & !is.matrix(test_points)){
+    stop(paste("points must be a dataframe or matrix", "\n"))
+  }
+  if (is.data.frame(test_points)) {
+    test_points <- as.matrix(test_points)
   }
  
   #Identify the simplicies that each grid point belongs 
   #First this is done by first computing the convex hull and testing if each of the grid points lies in the hull
-  hull <- convex_hull(points = inputPoint)
+  hull <- convex_hull(points = simplicies$input_points)
   
   #grid points in convex hull
-  inHull = in_convex_hull(hull, testPoint)
+  inHull = in_convex_hull(hull, test_points)
   
-  tri=as.matrix(tri)
+  tri_simplices=as.matrix(simplicies$simplices)
   
-  if(!is.matrix(testPoint))
+  if(!is.matrix(test_points))
   {
-    testPoint= as.matrix(testPoint)
-  }
-  if(!is.matrix(inputPoint))
-  {
-    inputPoint= as.matrix(inputPoint)
+    test_points= as.matrix(test_points)
   }
   
+  input_point= as.matrix(simplicies$input_points)
   
   #grid points outisde convex hull
-  gridOutside=which(inHull==FALSE)
+  outside_grid=which(inHull==FALSE)
   
-  testPoint[gridOutside,]=0.1 # set point outside the convex hull to 0.1 for now
+  test_points[outside_grid,]=0.1 # set point outside the convex hull to 0.1 for now
   
-  gridSpaceSimplex=NULL
+  grid_space_simplex=NULL
   
-  rowCount = nrow(testPoint)
+  rowCount = nrow(test_points)
   
   for(j in c(1:rowCount)){
-    if(testPoint[j,][1] != 0.1){
+    if(test_points[j,][1] != 0.1){
       k=1;
       
       #loop over the tri
-      for(i in c(1:length(tri[,1]))-1){
+      for(i in c(1:length(tri_simplices[,1]))-1){
         
         #get the point which makes the simplex
-        p1=tri[k,]
+        p1=tri_simplices[k,]
         p2=p1+1  # add 1 to each of the trigulation simplix before getting the points because R starts from 1
         
-        triPoint= inputPoint[p2,]
+        tri_point= input_point[p2,]
         
         
         #get the baycentric of this point
-        byPoint = rbind(c(testPoint[j,]))
+        by_point = rbind(c(test_points[j,]))
         
-        barycentricCoordinatesPoint = getBycentriCoordinate(triPoint,byPoint)
+        barycentric_coordinates_point = get_ny_centri_coordinate(tri_point,by_point)
         
         #the point is inside the triangle if the sum of the barycentricoordinate =1 and and are all positive
         
         #first, we check if the barycentri coordinate are all positive
-        checkSign = sign(barycentricCoordinatesPoint[1,])
+        check_sign = sign(barycentric_coordinates_point[1,])
         
         
         ##check if any of the barycentri are zero
-        zeroBarcentric=barycentricCoordinatesPoint[1,]==0
+        zero_ny_centri_coordinate=barycentric_coordinates_point[1,]==0
         
         #next, sum all  barycentri coordinate and if ==1
-        sumBarycentricCoordinatesPoint = rowSums(barycentricCoordinatesPoint)
+        sum_ny_centri_coordinate = rowSums(barycentric_coordinates_point)
         
         
         
         #If one or two barycentric coordinates are zero the point lies on the corresponding one or two edges of the element or if all are positive the point is inside.
-        
-        #if(length(which(zeroBarcentric==FALSE)) >0){
-        # print("here1")
-        #  print(t)
-        #  gridSpaceSimplex[j]= t
-        #  break    #point is inside the triangle, so break
-        #	}
-        if(length(which(checkSign==-1)) == 0 ){
-          gridSpaceSimplex[j]= i
+
+        if(length(which(check_sign ==-1)) == 0 ){
+          grid_space_simplex[j]= i
           break    #point is inside the triangle, so break
         } 
         else
-          gridSpaceSimplex[j]= -1 #point is outside the triangle
+          grid_space_simplex[j]= -1 #point is outside the triangle
         
         k<-k+1
         
@@ -113,9 +111,33 @@ find_simplex <- function(tri,inputPoint,testPoint) {
       
     }
     else{
-      gridSpaceSimplex[j]= -1 #point is outside the triangle
+      grid_space_simplex[j]= -1 #point is outside the triangle
     }
   }
-  return (gridSpaceSimplex)
-  #return(.Call("C_findSimplex", hull$convexhull, testPoints, PACKAGE="alphashape"))
+  return (grid_space_simplex)
+  #return(.Call("C_findSimplex", hull$convexhull, test_pointss, PACKAGE="alphashape"))
 }
+
+
+
+
+#Internal function use by \code{\link{findSimplex}} to calculate the bycentri co-ordinate
+get_ny_centri_coordinate <- function(X, P) {
+  M <- nrow(P)
+  N <- ncol(P)
+  if (ncol(X) != N) {
+    stop("Simplex X must have same number of columns as point matrix P")
+  }
+  if (nrow(X) != (N+1)) {
+    stop("Simplex X must have N columns and N+1 rows")
+  }
+  X1 <- X[1:N,] - (matrix(1,N,1) %*% X[N+1,,drop=FALSE])
+  if (rcond(X1) < .Machine$double.eps) {
+    warning("Degenerate simplex")
+    return(NULL)
+  }
+  Beta <- (P - matrix(X[N+1,], M, N, byrow=TRUE)) %*% solve(X1)
+  Beta <- cbind(Beta, 1 - apply(Beta, 1, sum))
+  return(Beta)
+}
+
