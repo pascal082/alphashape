57478f832e4e9ecea2d8015cd9259daa3c67f711
tretherington <tretherington@users.noreply.github.com>
2020-05-01 12:16:31 +1200

    Corrected find_simplex function

--- R/find-simplex.R
+++ R/find-simplex.R
@@ -11,7 +11,7 @@
 #'   \eqn{d}-dimensional space. 
 #' 
 #' @return A \eqn{n} length vector containing the index of the simplex the test 
-#' point is within, or a value of zero if a test point is not within any of the 
+#' point is within, or a value of NA if a test point is not within any of the 
 #' simplicies.
 #' 
 #' @examples 
@@ -25,7 +25,7 @@
 #' simplex <- find_simplex(simplicies = a_complex, test_points = p_test)
 #'  
 #' @export
-find_simplex <- function(simplicies,test_points) {
+find_simplex <- function(simplicies, test_points) {
   
   # Coerce the input to be matrix
   if(is.null(test_points)){
@@ -37,91 +37,72 @@ find_simplex <- function(simplicies,test_points) {
   if (is.data.frame(test_points)) {
     test_points <- as.matrix(test_points)
   }
- 
-  #Identify the simplicies that each grid point belongs 
-  #First this is done by first computing the convex hull and testing if each of the grid points lies in the hull
+
+  # Identify the simplicies that test point belongs 
+  # First check if the point lies in the convex hull
   hull <- convex_hull(points = simplicies$input_points)
+  inHull <- in_convex_hull(hull, test_points)
   
-  #grid points in convex hull
-  inHull = in_convex_hull(hull, test_points)
+  # Create an empty object to hold which simplex the test points are in
+  test_points_simplex <- NULL
   
-  tri_simplices=as.matrix(simplicies$simplices)
-  
-  if(!is.matrix(test_points))
-  {
-    test_points= as.matrix(test_points)
-  }
-  
-  input_point= as.matrix(simplicies$input_points)
-  
-  #grid points outisde convex hull
-  outside_grid=which(inHull==FALSE)
-  
-  test_points[outside_grid,]=0.1 # set point outside the convex hull to 0.1 for now
-  
-  grid_space_simplex=NULL
-  
-  rowCount = nrow(test_points)
-  
-  for(j in c(1:rowCount)){
-    if(test_points[j,][1] != 0.1){
-      k=1;
+  # For each test point that is within the convex hull
+  for (j in c(1:nrow(test_points))) {
+    if (inHull[j] == TRUE) {
+      k <- 1;
       
-      #loop over the tri
-      for(i in c(1:length(tri_simplices[,1]))-1){
+      # Loop over the tri
+      for(i in c(1:length(simplicies$simplices[, 1]))) {
         
-        #get the point which makes the simplex
-        p1=tri_simplices[k,]
-        p2=p1+1  # add 1 to each of the trigulation simplix before getting the points because R starts from 1
-        
-        tri_point= input_point[p2,]
+        # Get the point which makes the simplex
+        p1 <- simplicies$simplices[k, ]
+       
+        tri_point <- simplicies$input_points[p1, ]
         
         
         #get the baycentric of this point
         by_point = rbind(c(test_points[j,]))
         
-        barycentric_coordinates_point = get_ny_centri_coordinate(tri_point,by_point)
+        barycentric_coordinates_point <- get_ny_centri_coordinate(tri_point,by_point)
         
-        #the point is inside the triangle if the sum of the barycentricoordinate =1 and and are all positive
+        # The point is inside the triangle if the sum of the 
+        # barycentricoordinate = 1 and and are all positive
         
-        #first, we check if the barycentri coordinate are all positive
-        check_sign = sign(barycentric_coordinates_point[1,])
+        # Check if the barycentri coordinate are all positive
+        check_sign <- sign(barycentric_coordinates_point[1, ])
         
         
-        ##check if any of the barycentri are zero
-        zero_ny_centri_coordinate=barycentric_coordinates_point[1,]==0
+        # Check if any of the barycentric coordinates are zero
+        zero_ny_centri_coordinate <- barycentric_coordinates_point[1, ] == 0
         
-        #next, sum all  barycentri coordinate and if ==1
+        # Next, sum all  barycentric coordinates and if == 1
         sum_ny_centri_coordinate = rowSums(barycentric_coordinates_point)
         
-        
-        
-        #If one or two barycentric coordinates are zero the point lies on the corresponding one or two edges of the element or if all are positive the point is inside.
+        # If one or two barycentric coordinates are zero the point lies on the 
+        # corresponding one or two edges of the element or if all are positive 
+        # the point is inside.
 
-        if(length(which(check_sign ==-1)) == 0 ){
-          grid_space_simplex[j]= i
-          break    #point is inside the triangle, so break
+        if (length(which(check_sign == -1)) == 0 ) {
+          test_points_simplex[j] <- i
+          break    # point is inside the triangle, so break
         } 
         else
-          grid_space_simplex[j]= -1 #point is outside the triangle
+          test_points_simplex[j] = NA # test point is not in a simplex
         
-        k<-k+1
+        k <- k + 1
         
       }
       
     }
-    else{
-      grid_space_simplex[j]= -1 #point is outside the triangle
+    else {
+      test_points_simplex[j] = NA # test point is not in a simplex
     }
   }
-  return (grid_space_simplex)
+  return (test_points_simplex)
   #return(.Call("C_findSimplex", hull$convexhull, test_pointss, PACKAGE="alphashape"))
 }
 
-
-
-
-#Internal function use by \code{\link{findSimplex}} to calculate the bycentri co-ordinate
+# Internal function used by \code{\link{find_simplex}} to calculate the bycentri co-ordinate
 get_ny_centri_coordinate <- function(X, P) {
   M <- nrow(P)
   N <- ncol(P)
